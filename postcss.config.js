2e9766ad861851d9cde6c83f65a2cc0e101302ad
Sushruth Kumar <sush200506@gmail.com>
2025-12-13 03:49:55 +0530

    init

---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+<<<<<<< HEAD
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+=======
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+>>>>>>> 90f6320 (Extracted stack files)
